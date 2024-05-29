452024e3d5bc64cdf7d1ae0411fe6446b40e4468
leonelnexman <reshevdev@gmail.com>
2024-05-29 18:41:00 +0300

    nexman

--- src/js/components/mobile-animate.js
+++ src/js/components/mobile-animate.js
@@ -1,16 +1,20 @@
-// function toggleAnimations() {
-//     const elements = document.querySelectorAll('.animate__animated');
-//     if (window.innerWidth < 962) {
-//         elements.forEach(element => {
-//             element.classList.remove('animate__animated');
-//         });
-//     } else {
-//         elements.forEach(element => {
-//             element.classList.add('animate__animated');
-//         });
-//     }
-// }
+function removeAnimationClasses() {
+    const fadeInUpElements = document.querySelectorAll('.animate__fadeInUp');
+    const fadeInDownElements = document.querySelectorAll('.animate__fadeInDown');
+    const fadeInLeftElements = document.querySelectorAll('.animate__fadeInLeft');
 
-// document.addEventListener('DOMContentLoaded', toggleAnimations);
+    if (window.innerWidth < 962) {
+        fadeInUpElements.forEach(element => {
+            element.classList.remove('animate__fadeInUp');
+        });
+        fadeInDownElements.forEach(element => {
+            element.classList.remove('animate__fadeInDown');
+        });
+        fadeInLeftElements.forEach(element => {
+            element.classList.remove('animate__fadeInLeft');
+        });
+    }
+}
 
-// window.addEventListener('resize', toggleAnimations);
+document.addEventListener('DOMContentLoaded', removeAnimationClasses);
+window.addEventListener('resize', removeAnimationClasses);;
