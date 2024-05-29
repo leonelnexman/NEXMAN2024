4ae8ab517ae52c6ae0a87fe17380a9e6c9160f84
leonelnexman <reshevdev@gmail.com>
2024-05-29 21:50:44 +0300

    nexman

--- src/js/components/aos.js
+++ src/js/components/aos.js
@@ -9,17 +9,17 @@ AOS.init({
     duration: 1000,
 });
 
-function removeAOSClasses() {
-  if (window.innerWidth < 962) {
-      const elements = document.querySelectorAll('.aos-init, .aos-animate');
+// function removeAOSClasses() {
+//   if (window.innerWidth < 962) {
+//       const elements = document.querySelectorAll('.aos-init, .aos-animate');
       
-      elements.forEach(element => {
-          element.classList.remove('aos-init');
-          element.classList.remove('aos-animate');
-      });
-  }
-}
+//       elements.forEach(element => {
+//           element.classList.remove('aos-init');
+//           element.classList.remove('aos-animate');
+//       });
+//   }
+// }
 
-window.onload = removeAOSClasses;
+// window.onload = removeAOSClasses;
 
-window.onresize = removeAOSClasses;
+// window.onresize = removeAOSClasses;
