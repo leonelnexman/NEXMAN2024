7d2c683cf2d8bc39a55a016018c21555525587ea
leonelnexman <reshevdev@gmail.com>
2024-05-29 18:36:47 +0300

    nexman

--- src/js/components/mobile-animate.js
+++ src/js/components/mobile-animate.js
@@ -1,16 +1,16 @@
-function toggleAnimations() {
-    const elements = document.querySelectorAll('.animate__animated');
-    if (window.innerWidth < 962) {
-        elements.forEach(element => {
-            element.classList.remove('animate__animated');
-        });
-    } else {
-        elements.forEach(element => {
-            element.classList.add('animate__animated');
-        });
-    }
-}
+// function toggleAnimations() {
+//     const elements = document.querySelectorAll('.animate__animated');
+//     if (window.innerWidth < 962) {
+//         elements.forEach(element => {
+//             element.classList.remove('animate__animated');
+//         });
+//     } else {
+//         elements.forEach(element => {
+//             element.classList.add('animate__animated');
+//         });
+//     }
+// }
 
-document.addEventListener('DOMContentLoaded', toggleAnimations);
+// document.addEventListener('DOMContentLoaded', toggleAnimations);
 
-window.addEventListener('resize', toggleAnimations);
+// window.addEventListener('resize', toggleAnimations);
