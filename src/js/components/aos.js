bc9a26a6a2014fbfed75ded99df8de104e2648e3
leonelnexman <reshevdev@gmail.com>
2024-05-29 21:53:22 +0300

    nexman

--- src/js/components/aos.js
+++ src/js/components/aos.js
@@ -3,23 +3,7 @@ import AOS from 'aos';
 AOS.init();
 
 AOS.init({
-    // disable: 'mobile',
     offset: 120,
     delay: 0, 
     duration: 1000,
 });
-
-// function removeAOSClasses() {
-//   if (window.innerWidth < 962) {
-//       const elements = document.querySelectorAll('.aos-init, .aos-animate');
-      
-//       elements.forEach(element => {
-//           element.classList.remove('aos-init');
-//           element.classList.remove('aos-animate');
-//       });
-//   }
-// }
-
-// window.onload = removeAOSClasses;
-
-// window.onresize = removeAOSClasses;
