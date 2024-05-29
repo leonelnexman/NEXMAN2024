076bcc2bebc1f1e384e49f7f92c8895f81f8036c
leonelnexman <reshevdev@gmail.com>
2024-05-29 19:15:45 +0300

    nexman

--- src/js/components/mobile-animate.js
+++ src/js/components/mobile-animate.js
@@ -1,32 +1,50 @@
-function removeAnimationClasses() {
-    const fadeInUpElements = document.querySelectorAll('.animate__fadeInUp');
-    const fadeInDownElements = document.querySelectorAll('.animate__fadeInDown');
-    const fadeInLeftElements = document.querySelectorAll('.animate__fadeInLeft');
-    const WowDelete = document.querySelectorAll('.wow');
-    const ZoomInLeftElements = document.querySelectorAll('.animate__zoomIn');
-    const Animated = document.querySelectorAll('.animate__animated');
-
-    if (window.innerWidth < 962) {
-        fadeInUpElements.forEach(element => {
-            element.classList.remove('animate__fadeInUp');
-        });
-        fadeInDownElements.forEach(element => {
-            element.classList.remove('animate__fadeInDown');
-        });
-        fadeInLeftElements.forEach(element => {
-            element.classList.remove('animate__fadeInLeft');
-        });
-        ZoomInLeftElements.forEach(element => {
-            element.classList.remove('animate__zoomIn');
-        });
-        WowDelete.forEach(element => {
-            element.classList.remove('wow');
-        });
-        Animated.forEach(element => {
-            element.classList.remove('animate__animated');
-        });
+document.addEventListener("DOMContentLoaded", () => {
+    function checkScreenWidth() {
+        if (window.innerWidth < 962) {
+            const animateCssLink = document.getElementById("animateCss");
+            if (animateCssLink) {
+                animateCssLink.parentNode.removeChild(animateCssLink);
+            }
+        }
     }
-}
 
-document.addEventListener('DOMContentLoaded', removeAnimationClasses);
-window.addEventListener('resize', removeAnimationClasses);;
+    checkScreenWidth(); // Check on page load
+
+    window.addEventListener('resize', () => {
+        checkScreenWidth(); // Check on window resize
+    });
+});
+
+
+// function removeAnimationClasses() {
+//     const fadeInUpElements = document.querySelectorAll('.animate__fadeInUp');
+//     const fadeInDownElements = document.querySelectorAll('.animate__fadeInDown');
+//     const fadeInLeftElements = document.querySelectorAll('.animate__fadeInLeft');
+//     const WowDelete = document.querySelectorAll('.wow');
+//     const ZoomInLeftElements = document.querySelectorAll('.animate__zoomIn');
+//     const Animated = document.querySelectorAll('.animate__animated');
+
+//     if (window.innerWidth < 962) {
+//         fadeInUpElements.forEach(element => {
+//             element.classList.remove('animate__fadeInUp');
+//         });
+//         fadeInDownElements.forEach(element => {
+//             element.classList.remove('animate__fadeInDown');
+//         });
+//         fadeInLeftElements.forEach(element => {
+//             element.classList.remove('animate__fadeInLeft');
+//         });
+//         ZoomInLeftElements.forEach(element => {
+//             element.classList.remove('animate__zoomIn');
+//         });
+//         WowDelete.forEach(element => {
+//             element.classList.remove('wow');
+//         });
+//         Animated.forEach(element => {
+//             element.classList.remove('animate__animated');
+//         });
+//     }
+// }
+
+// document.addEventListener('DOMContentLoaded', removeAnimationClasses);
+// window.addEventListener('resize', removeAnimationClasses);;
