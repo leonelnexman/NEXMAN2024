1832490428ff50f690c2b5e154f9a7f80863c66e
leonelnexman <reshevdev@gmail.com>
2024-05-30 09:24:40 +0300

    nexman

--- src/js/components/cookie.js
+++ src/js/components/cookie.js
@@ -1,32 +1,8 @@
-function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = `expires=${  date.toUTCString()}`;
-    document.cookie = `${name  }=${  value  };${  expires  };path=/`;
+if (!localStorage.getItem('cookiesAccepted')) {
+    document.querySelector('.cookies').classList.add('visible');
 }
-
-function getCookie(name) {
-    const nameEQ = `${name  }=`;
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-window.onload = function() {
-    const cookiesBanner = document.querySelector('.cookies');
-    if (getCookie('cookiesAccepted')) {
-        cookiesBanner.classList.add('hidden');
-    } else {
-        cookiesBanner.classList.remove('hidden');
-    }
-};
 
 document.querySelector('.cookies__btn').addEventListener('click', () => {
-    const cookiesBanner = document.querySelector('.cookies');
-    cookiesBanner.classList.add('hidden');
-    setCookie('cookiesAccepted', 'true', 365);
+    document.querySelector('.cookies').classList.remove('visible');
+    localStorage.setItem('cookiesAccepted', 'true');
 });
